c352867bb152c719af99ceb4475ef48a29dc45a2
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-05-05 19:46:35 +0200

    progetto laravel pagine

--- Laravel001/progetto001/routes/web.php
+++ Laravel001/progetto001/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/stringa', function () {
+    return ('Ciao sono la pagina della stringa');
+});
+
+Route::get('/numero-intero', function () {
+    return 39;
+});
+
+Route::get('/array', function () {
+    return $arry=[12,3,14,20];
+});
